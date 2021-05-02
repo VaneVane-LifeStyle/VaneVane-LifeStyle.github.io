877f70ee120cb131596da46d5f8ad09824bef50d
ricardojdsg <ricardojdsg@gmail.com>
2021-05-01 20:04:13 -0400

    PWA IOS

--- app.js
+++ app.js
@@ -1,5 +1,28 @@
 
 // Registering Service Worker
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('sw.js');
-  }
+    //navigator.serviceWorker.register('sw.js');
+   
+   navigator.serviceWorker.register("sw.js").then(registration => {
+      console.log("success!");
+      if (registration.installing) {
+           registration.installing.postMessage("Howdy from your installing page.");
+      }
+   }, err => {
+      console.error("Installing the worker failed!", err);
+   });
+    
+}
+
+// Detects if device is on iOS 
+const isIos = () => {
+   const userAgent = window.navigator.userAgent.toLowerCase();
+   return /iphone|ipad|ipod/.test( userAgent );
+ }
+ // Detects if device is in standalone mode
+ const isInStandaloneMode = () => ('standalone' in window.navigator) && (window.navigator.standalone);
+ 
+ // Checks if should display install popup notification:
+ if (isIos() && !isInStandaloneMode()) {
+   this.setState({ showInstallMessage: true });
+ }
